4ebb69aab88c951c07c81ebc663a2fa3597ce82f
Todd Kerpelman <tkerpelman@plaid.com>
2023-12-11 09:50:06 -0800

    Pivoting door and delays

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -18,13 +18,13 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
           <VisuallyHidden>Dismiss menu</VisuallyHidden>
         </CloseButton>
         <Filler />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
+        <Nav >
+          <NavLink href="/sale" style={{"--delay-bit": "400ms"}}>Sale</NavLink>
+          <NavLink href="/new" style={{"--delay-bit": "450ms"}}>New&nbsp;Releases</NavLink>
+          <NavLink href="/men" style={{"--delay-bit": "500ms"}}>Men</NavLink>
+          <NavLink href="/women"style={{"--delay-bit": "550ms"}}>Women</NavLink>
+          <NavLink href="/kids"style={{"--delay-bit": "600ms"}}>Kids</NavLink>
+          <NavLink href="/collections" style={{"--delay-bit": "650ms"}}>Collections</NavLink>
         </Nav>
         <Footer>
           <SubLink href="/terms">Terms and Conditions</SubLink>
@@ -37,14 +37,7 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
 };
 
 const Overlay = styled(DialogOverlay)`
-  @keyframes fadein {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
+
   position: fixed;
   top: 0;
   left: 0;
@@ -55,6 +48,7 @@ const Overlay = styled(DialogOverlay)`
   display: flex;
   justify-content: flex-end;
   animation: fadein .2s ease-in-out backwards;
+  perspective: 3500px;
  
 `;
 
@@ -69,34 +63,30 @@ const Content = styled(DialogContent)`
     }
   }
 
+  @keyframes door-open {
+    0% {
+      transform: rotateY(-100deg);
+    }
+    100% {
+      transform: rotateY(0);
+    }
+  }
+
   background: white;
   width: 300px;
   height: 100%;
   padding: 24px 32px;
   display: flex;
   flex-direction: column;
+  transform-origin: 100% 5%;
   animation-delay: 0.2s;
-  animation-duration: 0.35s;
-  animation-timing-function: cubic-bezier(0,.4,.65,.99);
+  animation-duration: .35s;
+  animation-timing-function:linear;
   animation-fill-mode: backwards;
-  animation-name: slidein;
+  animation-name: door-open;
 
-  // Children
-  > * + * {
-    @keyframes fadein {
-      0% {
-        opacity: 0;
-      }
-      100% {
-        opacity: 1;
-      }
-    }
-    animation-delay: 0.4s;
-    animation-duration: 0.2s;
-    animation-timing-function: ease-in-out;
-    animation-fill-mode: backwards;
-    animation-name: fadein;
-  }
+
+
 
 `;
 
@@ -113,6 +103,8 @@ const Nav = styled.nav`
   display: flex;
   flex-direction: column;
   gap: 16px;
+
+  }  
 `;
 
 const NavLink = styled.a`
@@ -125,6 +117,20 @@ const NavLink = styled.a`
   &:first-of-type {
     color: var(--color-secondary);
   }
+
+    @keyframes fadein {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+    animation-delay: var(--delay-bit);
+    animation-duration: 0.2s;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: backwards;
+    animation-name: fadein;  
 `;
 
 const Filler = styled.div`
